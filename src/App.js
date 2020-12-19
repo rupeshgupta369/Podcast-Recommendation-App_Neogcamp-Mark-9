246f9138733811a129425288cd2c5c78a790f1f5
Rupesh Gupta <rupeshgupta369@gmail.com>
2020-12-19 21:59:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,170 @@
+import React, { useState } from "react";
+import "./styles.css";
+import { podcastDataCollection } from "./podcastData";
+
+var podcastKey = Object.keys(podcastDataCollection);
+
+export default function App() {
+  // var [song, setSong] = useState("Excited");
+  var [podcast, setPodcast] = useState("TopPodcast");
+
+  // function songClickHandler(song) {
+  function podcastClickHandler(podcast) {
+    // setSong(song);
+    setPodcast(podcast);
+  }
+
+  return (
+    <div className="App">
+      {/* Heading or Navigation Bar */}
+      <nav class="navigation">
+        <div class="nav-brand">Podcast Recommendation App</div>
+      </nav>
+
+      {/* Image section */}
+      <section class="img-section">
+        <img
+          src="https://image.freepik.com/free-vector/podcast-concept-illustration-concept_23-2148659355.jpg"
+          alt="podcast img"
+        />
+        <h2>
+          "A podcast is a great way to develop relationships with hard-to-reach
+          people."{" "}
+          <small>
+            - <em>Tim Paige</em>
+          </small>
+        </h2>
+      </section>
+
+      {/*  */}
+      <div>
+        {podcastKey.map((podcast) => (
+          <button
+            onClick={() => podcastClickHandler(podcast)}
+            style={{
+              cursor: "pointer",
+              padding: "1rem 1rem",
+              margin: "1rem 0.5rem",
+              borderRadius: "0.5rem"
+            }}
+          >
+            {" "}
+            {podcast}{" "}
+          </button>
+        ))}
+      </div>
+      <hr />
+
+      <section class="section">
+        <div
+          id="podcast-box"
+          style={{
+            textAlign: "center",
+            marginLeft: "center",
+            padding: "1.5rem"
+          }}
+        >
+          <ul>
+            {podcastDataCollection[podcast].map((parameter) => (
+              <li
+                id="podcast-link"
+                key={parameter.name}
+                style={{
+                  padding: "1rem",
+                  border: "1px solid border",
+                  borderRadius: "0.5rem",
+                  // width: "85%",
+                  // margin: "1rem 0rem",
+                  listStyle: "none",
+                  // innerHeight: "10rem",
+                  // outerHeight: "",
+                  textDecoration: "none"
+                }}
+              >
+                {" "}
+                <div style={{ fontSize: "larger" }}> {parameter.name} </div>
+                <div class="spotify-link" style={{ fontSize: "smaller" }}>
+                  {" "}
+                  <a href={parameter.link} target={"_blank"}>
+                    {parameter.link}
+                  </a>
+                  {/* <button>Spotify Link<a href={parameter.link} target={"_blank"}>{parameter.link}</a></button> */}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+
+      {/*  */}
+      {/* Footer Section */}
+      <footer>
+        <div id="footer-credit-connect">
+          <div id="footer-credit">Made by Rupesh Gupta</div>
+          <div id="footer-connect">Connect with me here!</div>
+        </div>
+
+        <div id="social-link">
+          <ul class="social-links list-non-bullet  icon-hover">
+            <li>
+              <a
+                class="link list-item-inline icon-hover"
+                href="https://rupeshgupta.netlify.app/"
+                target="_blank"
+              >
+                <i class="bx bx-world"></i>
+              </a>{" "}
+            </li>
+
+            <li>
+              <a
+                class="link list-item-inline icon-hover"
+                href="https://github.com/rupeshgupta369"
+                target="_blank"
+              >
+                <i class="bx bxl-github"></i>
+              </a>{" "}
+            </li>
+
+            <li>
+              <a
+                class="link list-item-inline icon-hover"
+                href="https://dev.to/rupeshgupta369"
+                target="_blank"
+              >
+                <i class="bx bxl-dev-to"></i>
+              </a>{" "}
+            </li>
+            <li>
+              <a
+                class="link list-item-inline icon-hover"
+                href="https://twitter.com/rupeshgupta369"
+                target="_blank"
+              >
+                <i class="bx bxl-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                class="link list-item-inline icon-hover"
+                href="https://www.linkedin.com/in/rupeshgupta369/"
+                target="_blank"
+              >
+                <i class="bx bxl-linkedin"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                class="link list-item-inline icon-hover"
+                href="https://www.instagram.com/rupeshgupta369/"
+                target="_blank"
+              >
+                <i class="bx bxl-instagram"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  );
+}
---
+++ src/podcastData.js
@@ -0,0 +1,97 @@
+export const podcastDataCollection = {
+  TopPodcast: [
+    {
+      name: "The Ranveer Show",
+      link: "https://open.spotify.com/show/6ZcvVBPQ2ToLXEWVbaw59P"
+    },
+
+    {
+      name: "Maha Bharat with Dhruv Rathee",
+      link: "https://open.spotify.com/show/0naX2ZM0GZnsqQZxVHTYhB"
+    },
+
+    {
+      name: "The Joe Rogan Experience",
+      link: "https://open.spotify.com/show/4rOoJ6Egrf8K2IrywzwOMk"
+    },
+
+    {
+      name: "On Purpose with Jay Shetty",
+      link: "https://open.spotify.com/show/5EqqB52m2bsr4k1Ii7sStc"
+    }
+  ],
+
+  Educational: [
+    {
+      name: "Josh Talks",
+      link: "https://open.spotify.com/show/4JIZGy84xwbv0Ui883u2Mr"
+    },
+
+    {
+      name: "Mission ISRO with Harsha Bhogle",
+      link: "https://open.spotify.com/show/2JXFCMLGVhTBtdz1WYxd4H"
+    },
+
+    {
+      name: "Radio City - Bharat Ki Amar Kahaniyaan",
+      link: "https://open.spotify.com/show/5kwtpnUDQEff95r55gFSp2"
+    },
+
+    {
+      name: "TED Talks Daily",
+      link: "https://open.spotify.com/show/1VXcH8QHkjRcTCEd88U3ti"
+    }
+  ],
+
+  Software: [
+    {
+      name: "Developer Duvidha",
+      link:
+        "https://open.spotify.com/show/5PabW1rGCv7tKelY8JOCm3?si=wLiCx8euRdKJIACjzpOFIw"
+    },
+
+    {
+      name: "thinkingwithtanay",
+      link:
+        "https://open.spotify.com/show/4IZCzLuBT6QezvViXlOgxO?si=A3lF_gdiRJ6-Ka56T6fqLg"
+    },
+
+    {
+      name: "Start Here: Web Development",
+      link:
+        "https://open.spotify.com/show/3V8TSvhtTRohclApkh2xIu?si=dsstGgS6QCii9I3C1xIAvw"
+    },
+
+    {
+      name: "AWS Podcast",
+      link:
+        "https://open.spotify.com/show/363iMcjThX5KNpfSHPRAjj?si=v1MaXqjTTheYU_n84NvKDw"
+    }
+  ],
+
+  Finance: [
+    {
+      name: "The GaryVee Audio Experience",
+      link:
+        "https://open.spotify.com/show/6SZVsPIxPfVs6aavqM1peY?si=KvilMrGMQrWK5cjbeEYwCw"
+    },
+
+    {
+      name: "The Personal Finance Podcast",
+      link:
+        "https://open.spotify.com/show/1zVBTDKimC3UoOQy59KDrR?si=szsNHWkdQ6KCziPyOjqx2w"
+    },
+
+    {
+      name: "Zerodha Educate",
+      link:
+        "https://open.spotify.com/show/1xppPkauvbTbDd5RXF6Gha?si=UbuJePpFSJqnMeuSKd7KsA"
+    },
+
+    {
+      name: "Sharekhan",
+      link:
+        "https://open.spotify.com/show/39RH6oEuziIY2GPb6Gwyfe?si=AO6v_mI6Tyu0K4xaOx4TWA"
+    }
+  ]
+};
